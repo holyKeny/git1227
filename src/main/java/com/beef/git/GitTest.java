44ceee12db8ae603efb227ee33d021c23e7cd316
beef <beefprofessional@163.com>
2022-05-11 21:27:33 +0800

    second commit

--- src/main/java/com/beef/git/GitTest.java
+++ src/main/java/com/beef/git/GitTest.java
@@ -7,5 +7,6 @@ package com.beef.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("git test");
+        System.out.println("git test1");
     }
 }
