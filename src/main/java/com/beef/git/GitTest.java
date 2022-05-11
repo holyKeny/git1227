84c0204f9a587b2ea1a7bacbc7fff5b61c349a2c
beef <beefprofessional@163.com>
2022-05-11 22:31:05 +0800

    master 5th commit

--- src/main/java/com/beef/git/GitTest.java
+++ src/main/java/com/beef/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("git test");
         System.out.println("git test1");
         System.out.println("git test3");
+        System.out.println("master 5th commit");
     }
 }
