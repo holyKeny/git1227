7c8ccd9235decf4801637aa49d3966e95623f365
beef <beefprofessional@163.com>
2022-05-11 21:32:51 +0800

    fourth commit

---
+++ src/main/java/com/beef/git/GitTest2.java
@@ -0,0 +1,11 @@
+package com.beef.git;
+
+/**
+ * @author lx
+ * @date 2022/5/11 - 21:31
+ */
+public class GitTest2 {
+    public static void main(String[] args) {
+        System.out.println("gittest222");
+    }
+}
