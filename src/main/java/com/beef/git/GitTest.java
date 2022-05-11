8ed55f9d6104caf2521a4e28ca185383b6025292
beef <beefprofessional@163.com>
2022-05-11 21:21:50 +0800

    first commit

---
+++ src/main/java/com/beef/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.beef.git;
+
+/**
+ * @author lx
+ * @date 2022/5/11 - 20:43
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("git test");
+    }
+}
